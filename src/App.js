e60cbc24892a1fec403776e25f93ae6e9c598297
Sofiene-Saguem <Sofiene.saguem2@gmail.com>
2021-02-01 10:34:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import abc from './logo2.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+<h1 className="titleRed">Your name here</h1>
+
+
+
+<img src={abc} alt="abcj" />
+
+
+
+<img src="/logo1.png" alt="abcjv"/>
+
+       </div>
+
+<video width="320" height="240" controls>
+
+<source src="myVideo.mp4" type="video/mp4"/> 
+
+</video>
+        </div>
   );
 }
 
